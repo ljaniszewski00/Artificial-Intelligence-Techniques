94f839f2613c53d0653e03216ecbbac2f535009a
Vader20FF <ljaniszewski00@gmail.com>
2022-03-08 19:12:07 +0100

    - added program's introduction functions where user decides which algorithms he would like to use

--- Main/main.py
+++ Main/main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def intro():
+    while True:
+        print("------------------------------------------------------------------")
+        print("Artificial Intelligence Techniques")
+        print("Lukasz Janiszewski, Jakub Muszynski")
+        print()
+        print("Choose which exercise's algorithms you would like to use:")
+        print("Exercise 1 - Particle Swarm Optimization, Differential Evolution")
+        print("Exercise 2 - ")
+        print("Exercise 3 - ")
+        print()
+        user_choice = int(input("Your choice: "))
+        while user_choice not in [1, 2, 3]:
+            print("Please choose option 1 or 2 or 3!")
+            user_choice = int(input("\nYour choice: "))
+        if user_choice == 1:
+            exercise1_intro()
+        elif user_choice == 2:
+            sys.exit()
+        elif user_choice == 3:
+            sys.exit()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def exercise1_intro():
+    print("------------------------------------------------------------------")
+    print("Exercise 1 - Particle Swarm Optimization, Differential Evolution")
+    print("------------------------------------------------------------------")
+    variables_number = int(input("\nChoose variables number: "))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+intro()
+
